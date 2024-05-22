b9bb08a86416c6d522d115c9419a38285589600a
REDGROUL <redgroulman@gmaio.com>
2024-05-22 19:30:20 +0300

    Маппер для перевода юзера в json и обратно

---
+++ Mappers/UserMapper.cs
@@ -0,0 +1,39 @@
+﻿using HotelSelect.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HotelSelect.Mappers
+{
+    public class UserMapper
+    {
+        public UserMapper()
+        {
+
+        }
+
+        public User MapJsonToUser(string userJson)
+        {
+            return JsonSerializer.Deserialize<User>(userJson); 
+        }
+
+        public string MapUserToJsonAndSave(User user)
+        {
+            string jsonUser = JsonSerializer.Serialize(user);
+
+            using (StreamWriter sw = new StreamWriter("Session.s"))
+            {
+                sw.WriteLine(jsonUser);
+            }
+
+            
+            return null;
+        }
+
+
+    }
+}
