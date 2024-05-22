4ff1a1207e75b823965e0ea83f11c7e9787dd438
REDGROUL <redgroulman@gmaio.com>
2024-05-22 20:12:57 +0300

    Revert "Update Hotel.cs"
    This reverts commit b3dafff12959bcbdd7c783f8d743064593b9c25d.

--- Entities/Hotel.cs
+++ Entities/Hotel.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,33 +8,13 @@ namespace HotelSelect.Entities
 {
     internal class Hotel
     {
-        long Id;
-        string Name;
-        int Countr_id;
-        long Sity_id;
-        int Stars;
-        string Adress;
-        string Phone_number;
-        string Description;
-
-        public Hotel(string name, int countr_id, int sity_id, int stars, string adress, string phone_number, string descript)
-        {
-            Name = name;
-            Countr_id = countr_id;
-            Sity_id = sity_id;
-            Stars = stars;
-            Adress = adress;
-            Phone_number = phone_number;
-            Description = descript;
-        }
-
-        public long id { get { return Id; } set { Id = value; } }
-        public string name { get { return Name; } set { Name = value; } }
-        public int countr_id { get { return Countr_id; } set { Countr_id = value; } }
-        public long sity_id { get { return Sity_id; } set { Sity_id = value; } }
-        public int stars { get { return Stars; } set { Stars = value; } }
-        public string adress { get { return Adress; } set { Adress = value; } }
-        public string phone_number { get { return Phone_number; } set { Phone_number = value; } }
-        public string description { get { return Description; } set {  Description = value; } }
+        public long Id { get; set; }
+        public int CountryId { get; set; }
+        public long CityId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Stars { get; set; }
+        public string Adress { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
