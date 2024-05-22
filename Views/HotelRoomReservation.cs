b85403cc68c4e8f060173c5bdc362ee5ac063d7c
Михаил Башкин <bashkin1979@gmail.com>
2024-05-22 20:21:33 +0300

    тестовые изменения

--- Views/HotelRoomReservation.cs
+++ Views/HotelRoomReservation.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using HotelSelect.Dao.repository;
+using HotelSelect.Entities;
+using HotelSelect.Entity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,8 +19,27 @@ namespace HotelSelect
         public HotelRoomReservation()
         {
             InitializeComponent();
+
+
+            SqlConnection sqlConnection = ConnectorDataBaseMicrosoftSQL.StartConnection().SqlConnection;
+            sqlConnection.Open();
+
+            string sqlQueryCountries = "SELECT * FROM Countries";
+
+            SqlCommand sqlCommandFindCountries = new SqlCommand(sqlQueryCountries, sqlConnection);
+            SqlDataReader sqlDataReader = sqlCommandFindCountries.ExecuteReader();
+            
+            while (sqlDataReader.Read())
+            {
+                ComBoxCountry.Items.Add((string)sqlDataReader.GetValue(1));
+                
+            }
+            sqlConnection.Close();
+
+
+
         }
 
-       
+
     }
 }
