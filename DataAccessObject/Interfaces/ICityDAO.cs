a86bc74a2411a0abae5f9cb74c01c5e30261d31f
REDGROUL <redgroulman@gmaio.com>
2024-05-27 20:54:44 +0300

    Форма бронирования отеля, выбор страны и выбор города по стране

---
+++ DataAccessObject/Interfaces/ICityDAO.cs
@@ -0,0 +1,14 @@
+﻿using HotelSelect.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSelect.DataAccessObject.Interfaces
+{
+    internal interface ICityDAO
+    {
+        List<City> GetAllCitiesByCountryId(string country);
+    }
+}
---
+++ DataAccessObject/Interfaces/ICountryDAO.cs
@@ -0,0 +1,17 @@
+﻿using HotelSelect.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSelect.DataAccessObject.Interfaces
+{
+    interface ICountryDAO
+    {
+        Country GetCountryById(long id);
+
+        List<Country> GetAllCountries();
+    }
+}
